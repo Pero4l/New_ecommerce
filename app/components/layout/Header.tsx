10caace1ea3d4908692e1506df315e225b2fd953
pero4l <lilpero03@gmail.com>
2025-10-27 23:59:46 +0100

    i

--- app/components/layout/Header.tsx
+++ app/components/layout/Header.tsx
@@ -13,7 +13,6 @@ export default function Header() {
 
   const isActive = (path: string): boolean => pathname === path;
 
-  
   return (
     <header className="bg-white shadow-sm sticky top-0 z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
